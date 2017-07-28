b6425cdc58a295d7c4a1833129c492d50c865807
Kyle Gundry <Gundr1kt@gmail.com>
2017-07-28 11:35:59 -0500

    Add new and create routes

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -3,4 +3,17 @@ class TheatersController < ApplicationController
     @theaters = Theater.all
     render "index.html.erb"
   end 
+
+  def new
+    @theater = Theater.new
+    render "new.html.erb"
+  end
+
+  def create
+    @theater = Theater.create(
+      name: params[:name],
+      seats: params[:seats]
+      )
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/" => "theaters#index"
+  get "/theaters/new" => "theaters#new"
+  post "/theaters" => "theaters#create"
 end
